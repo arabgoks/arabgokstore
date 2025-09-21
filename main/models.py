2b3313fdd71b1dd4e8dc00d775adb84b6baadd45
arabgoks <muhhafizh2510@gmail.com>
2025-09-21 16:53:16 +0700

    tugas 4 selesai

--- main/models.py
+++ main/models.py
@@ -1,6 +1,7 @@
 from django.utils import timezone
 from django.db import models
 import uuid
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     CATEGORY_CHOICES = [
@@ -23,6 +24,7 @@ class Product(models.Model):
     brand = models.CharField(max_length=255, blank=True, null=True)
     product_views = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
